e8d0d26e4fae0833b794997db089af88763de4d2
toonnyy8 <toonnyy8@gmail.com>
2019-01-29 17:09:33 +0800

    .

--- index.js
+++ index.js
@@ -3,22 +3,9 @@ const app = new koa();
 const path = require('path');
 const serve = require('koa-static');
 
-const main = serve(path.join(__dirname));
+const main = serve(path.join(__dirname + "/build/"));
 
 app.use(main);
 app.listen(8080);
 
 var robot = require("robotjs");
-
-// Speed up the mouse.
-robot.setMouseDelay(2);
-
-var twoPI = Math.PI * 2.0;
-var screenSize = robot.getScreenSize();
-var height = (screenSize.height / 2) - 10;
-var width = screenSize.width;
-
-for (var x = 0; x < width; x++) {
-    y = height * Math.sin((twoPI * x) / width) + height;
-    robot.moveMouse(x, y);
-}
