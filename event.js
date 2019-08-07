280655e246b4f85d86040b603eb75b3ee8e30d36
toonnyy8 <toonnyy8@gmail.com>
2019-08-07 20:51:38 +0800

    update

--- event.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.create({
-        url: "/build/index.html"
-    }, function (tab) {
-        console.log('window open');
-    });
-});
-
