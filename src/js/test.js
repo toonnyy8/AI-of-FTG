b2985c51bfe10d677071611a3f4c1e29bda4d562
toonnyy8 <toonnyy8@gmail.com>
2019-08-17 17:39:28 +0800

    spacevim

--- src/js/test.js
+++ src/js/test.js
@@ -1,6 +1,9 @@
-import * as tf from "@tensorflow/tfjs"
-import { LayerNormalization } from "./MirageNet/transformerXL/model"
-let a_ = new LayerNormalization({ axis: 0 })
+// import * as tf from "@tensorflow/tfjs"
+import * as tf from "@tensorflow/tfjs/dist/tf"
+tf
+
+import { positionwiseFF } from "./MirageNet/transformerXL/model"
+let a_ = new positionwiseFF({ axis: 0 })
 a_.apply(tf.tensor([1, 2, 3]))
-let a_2 = new LayerNormalization({ axis: 0 })
+let a_2 = new positionwiseFF({ axis: 0 })
 a_2.apply(tf.tensor([1, 2, 3]))
