f684c0fb50865037b70fbf3bb69f9af8a96dc74f
toonnyy8 <toonnyy8@gmail.com>
2019-08-31 14:49:10 +0800

    tokens

--- src/js/MirageNet/transformerXL/train.js
+++ src/js/MirageNet/transformerXL/train.js
@@ -18,7 +18,7 @@ export function modelFn(inp, tgt, nToken, FLAGS, initializer, projInitializer, i
                     decInp: inp,
                     target: tgt,
                     mems: mems,
-                    nToken: nToken,//23
+                    nToken: nToken,//46
                     nLayer: FLAGS.nLayer,
                     dModel: FLAGS.dModel,
                     dEmbed: FLAGS.dEmbed,
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,24 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
-// import * as game from "./game"
+import * as game from "./game"
+import * as tokens from "../param/tokens.json"
 // import * as model from "./model/model"
-import "./test"
+// import "./test"
 
-// let main = () => {
-//     console.log("finished")
-// }
+let main = () => {
+    console.log("finished")
+    console.log(tokens)
+    let getStatement = (actor = p1) => {
+    }
+}
 
-// let [p1, p2] = game.getPlayer()
-// let checkLoad = () => {
-//     if (p1 && p2) {
-//         main()
-//     } else {
-//         [p1, p2] = game.getPlayer()
-//         setTimeout(checkLoad, 100)
-//     }
-// }
-// setTimeout(checkLoad, 100)
+let [p1, p2] = game.getPlayer()
+let checkLoad = () => {
+    if (p1 && p2) {
+        main()
+    } else {
+        [p1, p2] = game.getPlayer()
+        setTimeout(checkLoad, 100)
+    }
+}
+setTimeout(checkLoad, 100)
