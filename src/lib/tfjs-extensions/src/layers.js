b933cc5f9b09a7e4251905ee48f46fd06ac3b7dd
toonnyy8 <toonnyy8@gmail.com>
2019-09-06 00:19:39 +0800

    layer update

--- src/lib/tfjs-extensions/src/layers.js
+++ src/lib/tfjs-extensions/src/layers.js
@@ -1,5 +1,4 @@
 import * as tf from "@tensorflow/tfjs"
-import { TypeFlags } from "typescript";
 
 // https://github.com/GistNoesis/Wisteria/blob/master/tfjs/src/LayerNorm.js
 export class LayerNormalization extends tf.layers.Layer {
