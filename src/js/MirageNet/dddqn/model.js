ded9e9a50ba9684e4eccd9031ee7d34f282e2bfa
toonnyy8 <toonnyy8@gmail.com>
2019-11-02 16:23:35 +0800

    調整模型

--- src/js/MirageNet/dddqn/model.js
+++ src/js/MirageNet/dddqn/model.js
@@ -236,18 +236,25 @@ export class DDDQN {
             activation: "selu",
             padding: "same"
         }).apply(WSLayer)
-        // cnnLayer = tf.layers.batchNormalization({}).apply(cnnLayer)
+        cnnLayer = tf.layers.batchNormalization({}).apply(cnnLayer)
         cnnLayer = tf.layers.conv1d({
             filters: filters * 4,
             kernelSize: [1],
             activation: "selu",
             padding: "same"
         }).apply(cnnLayer)
-        // cnnLayer = tf.layers.batchNormalization({}).apply(cnnLayer)
+        cnnLayer = tf.layers.batchNormalization({}).apply(cnnLayer)
 
         cnnLayer = tf.layers.dropout({ rate: 0.1 }).apply(cnnLayer)
 
         while (1 <= cnnLayer.shape[1] / 2) {
+            cnnLayer = tf.layers.conv1d({
+                filters: filters,
+                kernelSize: [2],
+                activation: "selu",
+                padding: "same"
+            }).apply(cnnLayer)
+            cnnLayer = tf.layers.batchNormalization({}).apply(cnnLayer)
             cnnLayer = tf.layers.conv1d({
                 filters: filters,
                 kernelSize: [2],
@@ -264,14 +271,14 @@ export class DDDQN {
             activation: "selu",
             padding: "same"
         }).apply(cnnLayer)
-        // value = tf.layers.batchNormalization({}).apply(value)
+        value = tf.layers.batchNormalization({}).apply(value)
         value = tf.layers.conv1d({
             filters: filters * 4,
             kernelSize: [1],
             activation: "selu",
             padding: "same"
         }).apply(value)
-        // value = tf.layers.batchNormalization({}).apply(value)
+        value = tf.layers.batchNormalization({}).apply(value)
         value = tf.layers.conv1d({
             filters: actionNum,
             kernelSize: [1],
@@ -285,14 +292,14 @@ export class DDDQN {
             activation: "selu",
             padding: "same"
         }).apply(cnnLayer)
-        // A = tf.layers.batchNormalization({}).apply(A)
+        A = tf.layers.batchNormalization({}).apply(A)
         A = tf.layers.conv1d({
             filters: filters * 4,
             kernelSize: [1],
             activation: "selu",
             padding: "same"
         }).apply(A)
-        // A = tf.layers.batchNormalization({}).apply(A)
+        A = tf.layers.batchNormalization({}).apply(A)
         A = tf.layers.conv1d({
             filters: actionNum,
             kernelSize: [1],
