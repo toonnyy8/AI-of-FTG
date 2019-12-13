3aa9caa941b583002294850de079c4b4f585a3f0
toonnyy8 <toonnyy8@gmail.com>
2019-12-13 22:12:20 +0800

    update

--- build/prod_mode/sw.js
+++ build/prod_mode/sw.js
@@ -4,32 +4,31 @@ let CACHE_NAME = "cacheV1"
 var urlsToCache = [
     '/',
     './main.fb6bbcaf.js',
-    './agent.347b5559.js'
-];
+    './agent.347b5559.js',
+    './main.fb6bbcaf.js.map',
+    './agent.347b5559.js.map'
+]
 
 
 self.addEventListener("install", (e) => {
-    console.log(e)
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
+                console.log('Opened cache')
+                return cache.addAll(urlsToCache)
             })
-    );
+    )
 })
 
 self.addEventListener('fetch', (e) => {
-    console.log(e)
     e.respondWith(
         caches.match(e.request)
             .then((response) => {
                 // Cache hit - return response
                 if (response) {
-                    return response;
+                    return response
                 }
-                return fetch(e.request);
-            }
-            )
-    );
-});
+                return fetch(e.request)
+            })
+    )
+})
