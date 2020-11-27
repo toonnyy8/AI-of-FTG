d73037243d476118a54b73aa5fc41dab34b9bda8
toonnyy8 <toonnyy8@gmail.com>
2020-11-28 00:18:56 +0800

    update

--- src/engine-drive/ts/driver.ts
+++ src/engine-drive/ts/driver.ts
@@ -20,19 +20,34 @@ export const Driver = (
     ws: () => tf.Variable[]
     unk: () => tf.Variable
 } => {
-    let actEmbs = new Array(config.ctrlNum).fill(0)
+    let actEmbs = new Array(config.ctrlNum)
+        .fill(0)
         .map((_, idx) => tf.variable(tf.randomNormal([config.actionNum, config.dact]), true, `actEmb${idx}`))
     let unk = tf.variable(tf.randomNormal([config.dmodel + config.ctrlNum * config.dact]), true, "unk")
-    let posConv = tf.sequential({
+
+    let posConv1 = tf.sequential({
         layers: [
             tf.layers.inputLayer({ inputShape: [config.r, 1, config.dmodel + config.ctrlNum * config.dact] }),
-            tf.layers.separableConv2d({ filters: config.dmodel, kernelSize: [config.r, 1], padding: "valid" })
-        ]
+            tf.layers.separableConv2d({ filters: config.dmodel, kernelSize: [config.r, 1], padding: "valid" }),
+        ],
     })
+    let posConv2 = tf.sequential({
+        layers: [
+            tf.layers.inputLayer({ inputShape: [config.r, 1, config.dmodel + config.ctrlNum * config.dact] }),
+            tf.layers.separableConv2d({ filters: config.dmodel, kernelSize: [config.r, 1], padding: "valid" }),
+        ],
+    })
+
     let mha1 = MHA(config.dmodel, config.head, config.dk, config.dv)
     let ff1 = FF(config.dmodel, config.dmodel * 2)
+
+    let memNum = 64
+    let memSize = 200
+    let extMem = tf.variable(tf.randomNormal([1, memNum, memSize]), true, "extMem")
+
     let mha2 = MHA(config.dmodel, config.head, config.dk, config.dv)
     let ff2 = FF(config.dmodel, config.dmodel * 2)
+    let ff3 = FF(config.dmodel, config.dmodel * 2)
 
     return {
         // ctrlActions:number[ctrls][len]
@@ -46,38 +61,44 @@ export const Driver = (
                 let inp = unk
                     .reshape([1, 1, 1, -1])
                     .tile([1, config.r - 1, 1, 1])
-                    .concat(
-                        tf.concat([
-                            <tf.Tensor4D>input.reshape([1, l, 1, h * w * c]),
-                            ...embs
-                        ], -1), 1)
-                let posOut = <tf.Tensor3D>(<tf.Tensor4D>posConv.apply(inp)).reshape([1, l, h * w * c])
+                    .concat(tf.concat([<tf.Tensor4D>input.reshape([1, l, 1, h * w * c]), ...embs], -1), 1)
 
-                let mha1Out = <tf.Tensor3D>mha1.fn(posOut, posOut)
-                // mha1Out = mha1Out.add(posOut)
-                let ff1Out = <tf.Tensor3D>ff1.fn(mha1Out)
-                // ff1Out = ff1Out.add(mha1Out)
+                let pos1Out = <tf.Tensor4D>(<tf.Tensor4D>posConv1.apply(inp)) //.reshape([1, l, h * w * c])
+                pos1Out = unk
+                    .reshape([1, 1, 1, -1])
+                    .tile([1, config.r - 1, 1, 1])
+                    .concat(tf.concat([pos1Out, ...embs], -1), 1)
+                let pos2Out = <tf.Tensor3D>(<tf.Tensor4D>posConv2.apply(pos1Out)).reshape([1, l, h * w * c])
+
+                let mha1Out = <tf.Tensor3D>mha1.fn(pos2Out, pos2Out)
+                mha1Out = mha1Out.add(pos2Out)
+                let ff1Out = nn.mish(<tf.Tensor3D>ff1.fn(mha1Out))
+                ff1Out = ff1Out.add(mha1Out)
 
                 let mha2Out = <tf.Tensor3D>mha2.fn(ff1Out, ff1Out)
-                // mha2Out = mha2Out.add(ff1Out)
-                let ff2Out = <tf.Tensor3D>ff2.fn(mha2Out)
-                // ff2Out = ff2Out.add(mha2Out)
+                mha2Out = mha2Out.add(ff1Out)
+                let ff2Out = nn.mish(<tf.Tensor3D>ff2.fn(mha2Out))
+                ff2Out = ff2Out.add(mha2Out)
 
-                return ff2Out.reshape([l, h, w, c])
+                let out = <tf.Tensor3D>ff3.fn(ff2Out)
+
+                return out.reshape([l, h, w, c])
             }),
         ws: () =>
             tf.tidy(() => [
                 ...actEmbs,
                 unk,
-                ...(<tf.Variable[]>posConv.getWeights()),
+                ...(<tf.Variable[]>posConv1.getWeights()),
+                ...(<tf.Variable[]>posConv2.getWeights()),
                 ...mha1.ws(),
                 ...ff1.ws(),
+                // extMem,
                 ...mha2.ws(),
                 ...ff2.ws(),
+                ...ff3.ws(),
             ]),
         unk: () => {
             return unk
-        }
+        },
     }
-
 }
--- src/engine-drive/ts/index.ts
+++ src/engine-drive/ts/index.ts
@@ -9,7 +9,9 @@ import { unstack } from "@tensorflow/tfjs"
 let canvas = <HTMLCanvasElement>document.getElementById("canvas")
 
 tf.setBackend("webgl").then(() => {
-    let h = 5, w = 10, dk = 4
+    let h = 5,
+        w = 10,
+        dk = 4
     let [{ fn: enc_fn, ws: enc_ws }, { fn: dec_fn, ws: dec_ws }] = AED({
         assetGroups: 8,
         assetSize: 16,
@@ -29,7 +31,141 @@ tf.setBackend("webgl").then(() => {
     let trainData: tf.Tensor4D = tf.ones([1, 1, 1, 1])
     let ctrl1: number[]
     let ctrl2: number[]
-        ; (<HTMLButtonElement>document.getElementById("load-data")).onclick = () => {
+    ;(<HTMLButtonElement>document.getElementById("load-data")).onclick = () => {
+        tf.tidy(() => {
+            let load = document.createElement("input")
+            load.type = "file"
+            load.accept = ".myz"
+
+            load.onchange = (event) => {
+                const files = <FileList>load.files
+                var reader = new FileReader()
+                reader.addEventListener("loadend", () => {
+                    myz.load(<ArrayBuffer>reader.result).then((datas) => {
+                        let frames = datas.find((data) => {
+                            return data.name == "frames"
+                        })
+                        let frameShape = datas.find((data) => {
+                            return data.name == "frameShape"
+                        })
+                        ctrl1 = Array.from(
+                            <Uint8Array>datas.find((data) => {
+                                return data.name == "ctrl1"
+                            })?.values
+                        )
+                        ctrl2 = Array.from(
+                            <Uint8Array>datas.find((data) => {
+                                return data.name == "ctrl2"
+                            })?.values
+                        )
+                        trainData.dispose()
+                        trainData = tf.keep(
+                            tf
+                                .tensor4d(
+                                    <Uint8Array>frames?.values,
+                                    <[number, number, number, number]>Array.from(<Uint8Array>frameShape?.values)
+                                )
+                                .cast("float32")
+                                .div(255)
+                        )
+                    })
+                })
+                reader.readAsArrayBuffer(files[0])
+            }
+
+            load.click()
+        })
+    }
+    const op = tf.train.adamax(0.001)
+    ;(<HTMLButtonElement>document.getElementById("train-log")).onclick = async () => {
+        let batchSize = 64
+        let t = 1
+        let batchStart = Math.round(Math.random() * (trainData.shape[0] - batchSize - t))
+        let train = () => {
+            for (let j = 0; j < 64; j++) {
+                tf.tidy(() => {
+                    let xImg = <tf.Tensor4D>trainData.slice([batchStart, 0, 0, 0], [batchSize, -1, -1, -1])
+                    let yImg = <tf.Tensor4D>trainData.slice([batchStart + t, 0, 0, 0], [batchSize, -1, -1, -1])
+                    let ctrl1Batch = ctrl1.slice(batchStart, batchStart + batchSize)
+                    let ctrl2Batch = ctrl2.slice(batchStart, batchStart + batchSize)
+                    // batch = tf.concat([batch, batch.reverse(2), batch.reverse(3), batch.reverse([2])])
+                    let x = <tf.Tensor4D>enc_fn(xImg)
+                    let y_enc = <tf.Tensor4D>enc_fn(yImg)
+                    let y_dec = <tf.Tensor4D>dec_fn(enc_fn(yImg))
+                    op.minimize(
+                        () => {
+                            let out_driver = <tf.Tensor4D>driver.fn(x, [ctrl1Batch, ctrl2Batch])
+                            let out_dec = <tf.Tensor4D>dec_fn(<tf.Tensor4D>out_driver)
+
+                            // return tf
+                            //     .sub(1, nn.ssim2d(y_dec, out_dec).mean())
+                            //     .add(tf.losses.meanSquaredError(y_enc, out_driver))
+
+                            // return tf.sub(1, nn.ssim2d(y_dec, out_dec).mean())
+                            // .add(tf.losses.meanSquaredError(y_enc, out_driver))
+
+                            // return tf.losses.meanSquaredError(y_dec.mul(255), out_dec.mul(255))
+                            return tf.losses.meanSquaredError(y_enc, out_driver)
+                        },
+                        true,
+                        <tf.Variable[]>(<unknown>[...driver.ws()])
+                    )?.print()
+                })
+
+                batchStart = Math.round(Math.random() * (trainData.shape[0] - batchSize - t))
+            }
+            tf.tidy(() => {
+                let test_xImg = trainData.slice([batchStart, 0, 0, 0], [batchSize, -1, -1, -1])
+                let test_yImg = <tf.Tensor3D>(
+                    dec_fn(
+                        enc_fn(trainData.slice([batchStart + batchSize + (t - 1), 0, 0, 0], [1, -1, -1, -1]))
+                    ).squeeze([0])
+                )
+                let ctrl1Batch = ctrl1.slice(batchStart, batchStart + batchSize)
+                let ctrl2Batch = ctrl2.slice(batchStart, batchStart + batchSize)
+                let test_out = <tf.Tensor3D>(
+                    tf.tidy(() =>
+                        (<tf.Tensor>dec_fn(driver.fn(<tf.Tensor4D>enc_fn(test_xImg), [ctrl1Batch, ctrl2Batch])))
+                            .slice([batchSize - 1, 0, 0, 0], [1, -1, -1, -1])
+                            .squeeze([0])
+                    )
+                )
+                let test_print = tf.concat([test_yImg, test_out], 1)
+
+                tf.browser.toPixels(test_print, canvas)
+                // test_xImg.dispose()
+                // test_yImg.dispose()
+                // test_out.dispose()
+                // test_print.dispose()
+            })
+            return tf.nextFrame()
+        }
+        for (let i = 0; i < 10; i++) {
+            await train()
+        }
+    }
+    ;(<HTMLButtonElement>document.getElementById("save-weights")).onclick = () => {
+        tf.tidy(() => {
+            let wList = [...enc_ws(), ...dec_ws()].reduce((acc, w) => {
+                return [
+                    ...acc,
+                    { name: w.name, values: <Float32Array>w.dataSync() },
+                    { name: `shape:${w.name}`, values: new Int32Array(w.shape) },
+                ]
+            }, [] as { name: string; values: Float32Array | Int32Array }[])
+
+            let blob = myz.save(wList)
+            let a = document.createElement("a")
+            let url = window.URL.createObjectURL(blob)
+            let filename = `weights.myz`
+            a.href = url
+            a.download = filename
+            a.click()
+            window.URL.revokeObjectURL(url)
+        })
+    }
+    ;(<HTMLButtonElement>document.getElementById("load-rendering-weights")).onclick = () => {
+        tf.tidy(() => {
             tf.tidy(() => {
                 let load = document.createElement("input")
                 load.type = "file"
@@ -39,29 +175,18 @@ tf.setBackend("webgl").then(() => {
                     const files = <FileList>load.files
                     var reader = new FileReader()
                     reader.addEventListener("loadend", () => {
-                        myz.load(<ArrayBuffer>reader.result).then((datas) => {
-                            let frames = datas.find((data) => {
-                                return data.name == "frames"
+                        myz.load(<ArrayBuffer>reader.result).then((wList) => {
+                            ;[...enc_ws(), ...dec_ws()].forEach((w) => {
+                                let values = <Float32Array>wList.find((_w) => _w.name == w.name)?.values
+                                let shape = <Int32Array>wList.find((_w) => _w.name == `shape:${w.name}`)?.values
+                                if (values) {
+                                    try {
+                                        w.assign(tf.tensor(values, Array.from(shape)))
+                                    } catch (e) {
+                                        console.error(e)
+                                    }
+                                }
                             })
-                            let frameShape = datas.find((data) => {
-                                return data.name == "frameShape"
-                            })
-                            ctrl1 = Array.from(<Uint8Array>datas.find((data) => {
-                                return data.name == "ctrl1"
-                            }).values)
-                            ctrl2 = Array.from(<Uint8Array>datas.find((data) => {
-                                return data.name == "ctrl2"
-                            }).values)
-                            trainData.dispose()
-                            trainData = tf.keep(
-                                tf
-                                    .tensor4d(
-                                        <Uint8Array>frames?.values,
-                                        <[number, number, number, number]>Array.from(<Uint8Array>frameShape?.values)
-                                    )
-                                    .cast("float32")
-                                    .div(255)
-                            )
                         })
                     })
                     reader.readAsArrayBuffer(files[0])
@@ -69,111 +194,8 @@ tf.setBackend("webgl").then(() => {
 
                 load.click()
             })
-        }
-    const op = tf.train.adamax(0.001)
-        ; (<HTMLButtonElement>document.getElementById("train-log")).onclick = async () => {
-            let batchSize = 64
-            let t = 3
-            let batchStart = Math.round(Math.random() * (trainData.shape[0] - batchSize - t))
-            let train = () => {
-                for (let j = 0; j < 64; j++) {
-                    tf.tidy(() => {
-                        let xImg = <tf.Tensor4D>trainData.slice([batchStart, 0, 0, 0], [batchSize, -1, -1, -1])
-                        let yImg = <tf.Tensor4D>trainData.slice([batchStart + t, 0, 0, 0], [batchSize, -1, -1, -1])
-                        let ctrl1Batch = ctrl1.slice(batchStart, batchStart + batchSize)
-                        let ctrl2Batch = ctrl2.slice(batchStart, batchStart + batchSize)
-                        // batch = tf.concat([batch, batch.reverse(2), batch.reverse(3), batch.reverse([2])])
-                        let x = <tf.Tensor4D>enc_fn(xImg)
-                        let y_enc = <tf.Tensor4D>enc_fn(yImg)
-                        let y_dec = <tf.Tensor4D>dec_fn(enc_fn(yImg))
-                        op.minimize(
-                            () => {
-                                let out_driver = <tf.Tensor4D>(driver.fn(x, [ctrl1Batch, ctrl2Batch]))
-                                let out_dec = dec_fn(<tf.Tensor4D>out_driver)
-
-                                // return tf.sub(1, nn.ssim2d(y, out).mean())
-                                return tf.losses.logLoss(y_dec, out_dec).add(tf.losses.meanSquaredError(y_enc, out_driver))
-                            },
-                            true,
-                            <tf.Variable[]>(<unknown>[...driver.ws()])
-                        )?.print()
-                    })
-
-                    batchStart = Math.round(Math.random() * (trainData.shape[0] - batchSize - t))
-                }
-                tf.tidy(() => {
-                    let test_xImg = trainData.slice([batchStart, 0, 0, 0], [batchSize, -1, -1, -1])
-                    let test_yImg = <tf.Tensor3D>dec_fn(enc_fn(trainData.slice([batchStart + batchSize + (t - 1), 0, 0, 0], [1, -1, -1, -1]))).squeeze([0])
-                    let ctrl1Batch = ctrl1.slice(batchStart, batchStart + batchSize)
-                    let ctrl2Batch = ctrl2.slice(batchStart, batchStart + batchSize)
-                    let test_out = <tf.Tensor3D>tf.tidy(() => (<tf.Tensor>dec_fn(driver.fn(<tf.Tensor4D>enc_fn(test_xImg), [ctrl1Batch, ctrl2Batch]))).slice([batchSize - 1, 0, 0, 0], [1, -1, -1, -1]).squeeze([0]))
-                    let test_print = tf.concat([test_yImg, test_out], 1)
-
-                    tf.browser.toPixels(test_print, canvas)
-                    // test_xImg.dispose()
-                    // test_yImg.dispose()
-                    // test_out.dispose()
-                    // test_print.dispose()
-                })
-                return tf.nextFrame()
-            }
-            for (let i = 0; i < 64; i++) {
-                await train()
-            }
-        }
-
-        ; (<HTMLButtonElement>document.getElementById("save-weights")).onclick = () => {
-            tf.tidy(() => {
-                let wList = [...enc_ws(), ...dec_ws()].reduce((acc, w) => {
-                    return [
-                        ...acc,
-                        { name: w.name, values: <Float32Array>w.dataSync() },
-                        { name: `shape:${w.name}`, values: new Int32Array(w.shape) },
-                    ]
-                }, [] as { name: string; values: Float32Array | Int32Array }[])
-
-                let blob = myz.save(wList)
-                let a = document.createElement("a")
-                let url = window.URL.createObjectURL(blob)
-                let filename = `weights.myz`
-                a.href = url
-                a.download = filename
-                a.click()
-                window.URL.revokeObjectURL(url)
-            })
-        }
-        ; (<HTMLButtonElement>document.getElementById("load-rendering-weights")).onclick = () => {
-            tf.tidy(() => {
-                tf.tidy(() => {
-                    let load = document.createElement("input")
-                    load.type = "file"
-                    load.accept = ".myz"
-
-                    load.onchange = (event) => {
-                        const files = <FileList>load.files
-                        var reader = new FileReader()
-                        reader.addEventListener("loadend", () => {
-                            myz.load(<ArrayBuffer>reader.result).then((wList) => {
-                                ;[...enc_ws(), ...dec_ws()].forEach((w) => {
-                                    let values = <Float32Array>wList.find((_w) => _w.name == w.name)?.values
-                                    let shape = <Int32Array>wList.find((_w) => _w.name == `shape:${w.name}`)?.values
-                                    if (values) {
-                                        try {
-                                            w.assign(tf.tensor(values, Array.from(shape)))
-                                        } catch (e) {
-                                            console.error(e)
-                                        }
-                                    }
-                                })
-                            })
-                        })
-                        reader.readAsArrayBuffer(files[0])
-                    }
-
-                    load.click()
-                })
-            })
-        }
+        })
+    }
     // ; (<HTMLButtonElement>document.getElementById("test")).onclick = () => {
     //     tf.tidy(() => {
     //         let batchSize = 1
@@ -190,30 +212,33 @@ tf.setBackend("webgl").then(() => {
     if (testFPS) {
         const H = 5,
             W = 10
-        const mha = MHA(H * W * dk, 8, 32, 32)
-        const ff = FF(H * W * dk, H * W * dk * 2)
-        const conv = tf.sequential({
-            layers: [
-                tf.layers.inputLayer({ inputShape: [64, 1, H * W * dk] }),
-                tf.layers.separableConv2d({ filters: H * W * dk, kernelSize: [8, 1], padding: "valid" }),
-            ],
-        })
+        // const mha = MHA(H * W * dk, 8, 32, 32)
+        // const ff = FF(H * W * dk, H * W * dk * 2)
+        // const conv = tf.sequential({
+        //     layers: [
+        //         tf.layers.inputLayer({ inputShape: [64, 1, H * W * dk] }),
+        //         tf.layers.separableConv2d({ filters: H * W * dk, kernelSize: [8, 1], padding: "valid" }),
+        //     ],
+        // })
         const tt = () =>
             tf.tidy(() => {
-                let test = tf.ones([1, 64, 1, H * W * dk])
-                test = tf.pad(test, [
-                    [0, 0],
-                    [7, 0],
-                    [0, 0],
-                    [0, 0],
-                ])
-                test = (<tf.Tensor>conv.apply(test)).reshape([1, 64, H * W * dk])
-                test = mha.fn(test, test)
-                test = <tf.Tensor>ff.fn(test)
-                test = mha.fn(test, test)
-                test = <tf.Tensor>ff.fn(test)
-                test = mha.fn(test, test).slice([0, 63, 0], [1, 1, -1])
-                test = (<tf.Tensor>ff.fn(test)).reshape([1, H, W, dk])
+                let test = <tf.Tensor4D>tf.ones([64, H, W, dk])
+                test = <tf.Tensor4D>(
+                    driver.fn(test, new Array(2).fill(new Array(64).fill(0))).slice([63, 0, 0, 0], [1, -1, -1, -1])
+                )
+                // test = tf.pad(test, [
+                //     [0, 0],
+                //     [7, 0],
+                //     [0, 0],
+                //     [0, 0],
+                // ])
+                // test = (<tf.Tensor>conv.apply(test)).reshape([1, 64, H * W * dk])
+                // test = mha.fn(test, test)
+                // test = <tf.Tensor>ff.fn(test)
+                // test = mha.fn(test, test)
+                // test = <tf.Tensor>ff.fn(test)
+                // test = mha.fn(test, test).slice([0, 63, 0], [1, 1, -1])
+                // test = (<tf.Tensor>ff.fn(test)).reshape([1, H, W, dk])
                 // let test = tf.ones([1, H, W, dk])
                 tf.browser.toPixels(<tf.Tensor3D>dec_fn(test).squeeze([0]), canvas)
                 requestAnimationFrame(tt)
