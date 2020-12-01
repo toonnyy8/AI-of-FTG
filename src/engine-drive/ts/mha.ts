a26068d5f2a06d1f26d11a203d526042741711e7
toonnyy8 <toonnyy8@gmail.com>
2020-12-01 12:36:01 +0800

    修正 look ahead mask

--- src/engine-drive/ts/mha.ts
+++ src/engine-drive/ts/mha.ts
@@ -32,39 +32,53 @@ export const positionalEncoding = (position: number, d_model: number) => {
     }
 }
 
-export const MHA = (d_model: number, h: number, d_k: number, d_v: number) => {
-    const QLinear = tf.layers.dense({ inputShape: [d_model], units: d_k * h })
-    const KVLinear = tf.layers.dense({ inputShape: [d_model], units: (d_k + d_v) * h })
+export const MHA = (d_model: number, h: number, dk: number, dv: number) => {
+    const QLinear = tf.layers.dense({ inputShape: [d_model], units: dk * h })
+    const KVLinear = tf.layers.dense({
+        inputShape: [d_model],
+        units: (dk + dv) * h,
+    })
 
-    const outLinear = tf.layers.dense({ inputShape: [d_v * h], units: d_model })
+    const outLinear = tf.layers.dense({ inputShape: [dv * h], units: d_model })
 
     return {
         fn: (Qin: tf.Tensor2D, KVin: tf.Tensor2D) =>
             tf.tidy(() => {
-                const mask = tf.linalg.bandPart(tf.fill([Qin.shape.slice(-2)[0], KVin.shape.slice(-2)[0]], -1e9), -1, 0)
+                const [Lq] = Qin.shape
+                const [Lkv] = KVin.shape
+                const mask = tf
+                    .sub(1, tf.linalg.bandPart(tf.ones([Lq, Lkv]), -1, 0))
+                    .mul(-1e9)
+                    .expandDims(-1)
 
                 let reshape2Head = (t: tf.Tensor2D) => {
                     const [l] = t.shape
                     return <tf.Tensor3D>t.reshape([l, h, -1]).transpose([1, 0, 2])
                 }
-                const Q = reshape2Head(<tf.Tensor2D>QLinear.apply(Qin))
+                const Q = <tf.Tensor2D>QLinear.apply(Qin)
 
-                const [K, V] = (<tf.Tensor>KVLinear.apply(KVin))
-                    .split([d_k * h, d_v * h], -1)
-                    .map((t) => reshape2Head(<tf.Tensor2D>t))
+                const [K, V] = <tf.Tensor2D[]>(<tf.Tensor>KVLinear.apply(KVin)).split([dk * h, dv * h], -1)
 
-                const att = tf
-                    .matMul(Q, K, false, true)
-                    .div(d_k ** 0.5)
-                    .add(mask)
-                    .softmax(-1)
+                const att = nn.softmax(
+                    tf
+                        .mul(Q.reshape([Lq, 1, h, dk]), K.reshape([1, Lkv, h, dk]))
+                        .sum(-1)
+                        .div(dk ** 0.5)
+                        .add(mask),
+                    1
+                )
 
                 let headConcat = (t: tf.Tensor) => {
                     const [, l] = t.shape
                     return t.transpose([1, 0, 2]).reshape([l, -1])
                 }
 
-                return <tf.Tensor2D>outLinear.apply(headConcat(tf.matMul(att, V)))
+                return <tf.Tensor2D>outLinear.apply(
+                    tf
+                        .mul(att.reshape([Lq, Lkv, h, 1]), V.reshape([1, Lkv, h, dv]))
+                        .sum(1)
+                        .reshape([Lq, h * dv])
+                )
             }),
         ws: () => <tf.Variable[]>[...QLinear.getWeights(), ...KVLinear.getWeights(), ...outLinear.getWeights()],
     }
