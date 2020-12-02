9d409833ec128a6ef969350d6f15ee531118e7ca
toonnyy8 <toonnyy8@gmail.com>
2020-12-02 12:50:01 +0800

    修改 driver 參數設定

--- src/engine-drive/ts/driver.ts
+++ src/engine-drive/ts/driver.ts
@@ -4,41 +4,50 @@ import * as layers from "./layers"
 import { MHA, FF } from "./mha"
 import { sequential } from "@tensorflow/tfjs"
 
-export const Driver = (
-    config: {
-        ctrlNum: number
-        actionNum: number
-        dact: number
-        dmodel: number
-        r: number
-        head: number
-        dk: number
-        dv: number
-    } = { ctrlNum: 2, actionNum: 2 ** 7, dact: 32, dmodel: 200, r: 8, head: 8, dk: 32, dv: 32 }
-): {
+export const Driver = (config: {
+    ctrlNum: number
+    actionNum: number
+    dmodel: number
+    dact?: number
+    r?: number
+    head?: number
+    dk?: number
+    dv?: number
+    hiddens?: number
+}): {
     fn: (input: tf.Tensor4D, actions: number[][]) => tf.Tensor
     ws: () => tf.Variable[]
     unk: () => tf.Variable
 } => {
-    let actEmbs = new Array(config.ctrlNum)
+    const ctrlNum = config.ctrlNum
+    const actionNum = config.actionNum
+    const dmodel = config.dmodel
+    const dact = config.dact !== undefined ? config.dact : 32
+    const r = config.r !== undefined ? config.r : 8
+    const head = config.head !== undefined ? config.head : 8
+    const dk = config.dk !== undefined ? config.dk : 32
+    const dv = config.dv !== undefined ? config.dv : 32
+    const hiddens = config.hiddens !== undefined ? config.hiddens : 400
+
+    let actEmbs = new Array(ctrlNum)
         .fill(0)
-        .map((_, idx) => tf.variable(tf.randomNormal([config.actionNum, config.dact]), true, `actEmb${idx}`))
-    let unk = tf.variable(tf.randomNormal([config.dmodel + config.ctrlNum * config.dact]), true, "unk")
+        .map((_, idx) => tf.variable(tf.randomNormal([actionNum, dact]), true, `actEmb${idx}`))
+    let unk = tf.variable(tf.randomNormal([dmodel + ctrlNum * dact]), true, "unk")
 
     let posConv1 = tf.sequential({
         layers: [
-            tf.layers.inputLayer({ inputShape: [config.r, 1, config.dmodel + config.ctrlNum * config.dact] }),
-            tf.layers.separableConv2d({ filters: config.dmodel, kernelSize: [config.r, 1], padding: "valid" }),
+            tf.layers.inputLayer({ inputShape: [r, 1, dmodel + ctrlNum * dact] }),
+            tf.layers.separableConv2d({ filters: dmodel, kernelSize: [r, 1], padding: "valid" }),
         ],
     })
 
-    let mha1 = MHA(config.dmodel, config.head, config.dk, config.dv)
-    let ff1 = FF(config.dmodel, config.dmodel * 2)
+    let mha1 = MHA(dmodel, head, dk, dv)
+    let ff1 = FF(dmodel, hiddens)
 
-    let mha2 = MHA(config.dmodel, config.head, config.dk, config.dv)
-    let ff2 = FF(config.dmodel, config.dmodel * 2)
+    let mha2 = MHA(dmodel, head, dk, dv)
+    let ff2 = FF(dmodel, hiddens)
 
-    let ffout = FF(config.dmodel, config.dmodel * 2)
+    let ffout = FF(dmodel, hiddens)
 
     const norm = <T extends tf.Tensor>(x: T) =>
         tf.tidy(() => {
@@ -52,13 +61,13 @@ export const Driver = (
         fn: (input: tf.Tensor4D, ctrlActions: number[][]) =>
             tf.tidy(() => {
                 let [l, h, w, c] = input.shape
-                if (config.dmodel != h * w * c) throw new Error("h*w*c != dmodel")
+                if (dmodel != h * w * c) throw new Error("h*w*c != dmodel")
                 let embs: tf.Variable<tf.Rank.R4>[] = ctrlActions.map((actions, idx) => {
-                    return <tf.Variable<tf.Rank.R4>>tf.gather(actEmbs[idx], actions, 0).reshape([1, l, 1, config.dact])
+                    return <tf.Variable<tf.Rank.R4>>tf.gather(actEmbs[idx], actions, 0).reshape([1, l, 1, dact])
                 })
                 let inp = unk
                     .reshape([1, 1, 1, -1])
-                    .tile([1, config.r - 1, 1, 1])
+                    .tile([1, r - 1, 1, 1])
                     .concat(tf.concat([<tf.Tensor4D>input.reshape([1, l, 1, h * w * c]), ...embs], -1), 1)
 
                 let pos1Out = <tf.Tensor2D>(<tf.Tensor4D>posConv1.apply(inp)).reshape([l, h * w * c])
